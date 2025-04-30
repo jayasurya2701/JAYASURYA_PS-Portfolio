34790dca76fde4bd70d461851fee774b4891b1be
JAYASURYA <112236021+jayasurya2701@users.noreply.github.com>
2025-04-30 23:05:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ elif page == "📄 Resume":
     st.header("📄 View & Download My Resume")
 
     # Google Drive File ID (Extract from your link)
-    file_id = "1cq2c-gH4nrd-Aj72mLibxWuIGNv"
+    file_id = "1cq2c-gH4nrd-Aj72mLibxWuIGNv-0pDs"
 
     # Correct Google Drive Embed & Download URLs
     embed_url = f"https://drive.google.com/file/d/{file_id}/preview"
